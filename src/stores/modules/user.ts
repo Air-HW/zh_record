b1c211c4345f9b9f3bcf1d85ff29cef578f51829
Zhang <1592955886@qq.com>
2023-10-13 10:12:14 +0800

    数据持久化

--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -2,7 +2,7 @@
  * @Author: 张书瑞
  * @Date: 2023-10-12 23:46:44
  * @LastEditors: 张书瑞
- * @LastEditTime: 2023-10-12 23:49:42
+ * @LastEditTime: 2023-10-13 00:47:30
  * @FilePath: \zh_record\src\stores\modules\user.ts
  * @Description: 
  * @email: 1592955886@qq.com
@@ -19,11 +19,17 @@ export const useUserStore = defineStore('user', {
   }),
   getters: {
     getUser(): User | null {
+      const userStr = localStorage.getItem('user');
+      if (userStr) {
+        return JSON.parse(userStr) as User;
+      }
       return this.user;
     }
   },
   actions: {
     setUser(user: User): void {
+      // 将用户信息写入到本地存储中
+      localStorage.setItem('user', JSON.stringify(user));
       this.user = user;
     }
   }
